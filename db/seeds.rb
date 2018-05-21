e26dadac1f1721fbaa08d2afa022144ad7e6963c
Nathan Lebas <nathanlebas3@gmail.com>
2018-05-21 16:21:53 +0200

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_1 = User.create(email: "henri@gmail.com", password: "test")
+
+Tool.create(name: "hammer", description: "brand new", user_id: user_1.id)
+Tool.create(name: "toolbox", description: "fully furinished", user_id: user_1.id)
