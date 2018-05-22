fc0bca9456e2b60ccef043dd1a64502c47b23160
Nathan Lebas <nathanlebas3@gmail.com>
2018-05-22 10:53:29 +0200

    new branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tools#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tools, only: [:index, :show, :new, :create, :destroy]
+  resources :tools, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
 end
