e0e850c66ecee8294abb22ca3fba96bc0bb0c991
Nathan Lebas <nathanlebas3@gmail.com>
2018-05-22 17:12:30 +0200

    the delete button is now only displayed to the current_user, if is the owner of the tool

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2018_05_22_142521) do
     t.string "category"
     t.string "picture_url"
     t.string "status"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.string "photo"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
