7e9b2180c7fdfb5a2334c596f60305646a0e6ea8
Johannes Lohner <johannes.lohner@hotmail.com>
2018-05-23 10:48:28 +0200

    update view index page

--- db/schema.rb
+++ db/schema.rb
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(version: 2018_05_23_080051) do
     t.string "category"
     t.string "picture_url"
     t.string "status"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.string "photo"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
