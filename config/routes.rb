f71050c690b1cc0a287738d6e78572113f7db537
Simon Hoeferlin <s@hoeferlins.de>
2018-05-23 11:32:52 +0200

    Address field and geolocating implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'tools#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tools, only: [:index, :show, :new, :create, :edit, :update, :destroy]
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   post "/bookings/:tool_id", to: "bookings#create", as: :bookings_create
   get "/bookings", to: "bookings#index"
+
+
 end
