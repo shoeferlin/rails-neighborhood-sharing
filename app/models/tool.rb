8d123e6118f60aab054b53dcbaaf916d637b07f9
Simon Hoeferlin <s@hoeferlins.de>
2018-05-24 11:23:45 +0200

    PGsearch basic works

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,4 +3,11 @@ class Tool < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   mount_uploader :photo, PhotoUploader
+
+  include PgSearch
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
